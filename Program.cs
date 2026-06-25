c598c24b025b1b4e593113dd0f7d9144da7133dd
lucas <lucas.r.s.s.2031@gmail.com>
2026-06-25 20:21:59 -0300

    feat: altera armazenamento para lista de filmes

--- Program.cs
+++ Program.cs
@@ -1,14 +1,45 @@
-﻿Filme filme = new Filme();
-Console.Write("Titulo: ");
-filme.titulo = Console.ReadLine();
+﻿List<Filme> listaFilmes = new List<Filme>();
+int opcao = 0;
 
-Console.Write("Gênero: ");
-filme.genero = Console.ReadLine();
+do 
+{
+    Console.WriteLine("\n=== catálogo de filmes ===");
+    Console.WriteLine("2 - Cadastrar filme");
+    Console.WriteLine("3 - exibir todos os filmes");
+    Console.WriteLine("1 - Sair");
 
-Console.WriteLine("Ano de lançamento: ");
-filme.anoLancamento = Convert.ToInt32(Console.Read());
+    switch (opcao)
+    {
+        case 2:
+            Filme Novofilme = new Filme();
+            Console.Write("Titulo: ");
+            Novofilme.titulo = Console.ReadLine();
 
-Console.WriteLine("classificação indicativa:");
-filme.classificarIndicativa = Convert.ToInt32(Console.Read());
+            Console.Write("Gênero: ");
+            Novofilme.genero = Console.ReadLine();
+
+            Console.WriteLine("Ano de lançamento: ");
+            Novofilme.anoLancamento = Convert.ToInt32(Console.Read());
+
+            Console.WriteLine("classificação indicativa:");
+            Novofilme.classificarIndicativa = Convert.ToInt32(Console.Read());
+            listaFilmes.Add(Novofilme);
+            Console.WriteLine("\nFilme cadastrado com sucesso!");
+            break;
+
+        case 3:
+            if (listaFilmes.Count == 0)
+            {
+                Console.WriteLine("\nNenhum filme cadastrado ainda.");
+            }
+            else
+            {
+                foreach (var filme in listaFilmes)
+            }
+            break;
+    }
+};
+
+
 
 filme.exibirInformacoes();
